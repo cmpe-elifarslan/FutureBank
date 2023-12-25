2a2c8970e290b6056e1c0840050fd6fcff674cd5
Elif Arslan <95655388+cmpe-elifarslan@users.noreply.github.com>
2023-12-25 14:38:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -219,14 +219,14 @@ if authentication_status == True:
             time.sleep(1)  
             st.write("calculating...")
             st.write("calculated:")
+            st.write("calculated:")
             if result[0] == 0:
                 st.subheader("no")
-                y='no'
+                db.change_data("no")
             elif result[0] == 1:
-                st.subheader("yes")
+                db.change_data("yes")
                 y='yes'
-            time.sleep(1)
-            db.change_data(age,job,marital,education, default,housing,loan,contact,month,day_of_week, duration,campaign,pdays, previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed,y)
+            time.sleep(1)            
             show=db.fetch_all_data() 
             df_print = pd.DataFrame(show)
             latest_row = df_print[df_print['time'] == df_print['time'].max()]
