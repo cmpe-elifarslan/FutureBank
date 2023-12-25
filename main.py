39e54bf99ce2b8dec8f723de60b565c186e67e41
Elif Arslan <95655388+cmpe-elifarslan@users.noreply.github.com>
2023-12-25 14:40:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,6 @@ if authentication_status == True:
             time.sleep(1)  
             st.write("calculating...")
             st.write("calculated:")
-            st.write("calculated:")
             if result[0] == 0:
                 st.subheader("no")
                 db.change_data("no")
