658f0042bcf17e91337218c571aa09b99ac654c7
Elif Arslan <95655388+cmpe-elifarslan@users.noreply.github.com>
2023-12-25 15:53:24 +0300

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ DETA_KEY = os.getenv("DETA_KEY")
 
 deta= Deta(DETA_KEY)
 db = deta.Base("clients")
-def insert_data(age,job,marital,education, default,housing,loan,contact,month,day_of_week, duration,campaign,pdays, previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed):
+def insert_data(age,job,marital,education, default,housing,loan,contact,month,day_of_week, duration,campaign,pdays, previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed,y):
     current_time = datetime.datetime.now().isoformat()
     return db.put({"age":age,"job":job,"marital":marital,"education":education,
                  "default":default,"housing":housing,"loan":loan,
@@ -16,8 +16,14 @@ def insert_data(age,job,marital,education, default,housing,loan,contact,month,da
                  "campaign":campaign,"pdays":pdays,"previous": previous,
                  "poutcome":poutcome,"emp.var.rate":emp_var_rate,"cons.price.idx":cons_price_idx,
                  "cons.conf.idx":cons_conf_idx,
-                 "euribor3m":euribor3m,"nr.employed":nr_employed,"time": current_time})
+                 "euribor3m":euribor3m,"nr.employed":nr_employed,"time": current_time,"y":y})
     
 def fetch_all_data():
     res= db.fetch()
     return res.items
+
+def change_data(age,job,marital,education, default,housing,loan,contact,month,day_of_week, duration,campaign,pdays, previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed,y):
+    all_items = db.fetch().items
+    last_item_id = all_items[-1]['key']
+    db.delete(last_item_id)
+    insert_data(age,job,marital,education, default,housing,loan,contact,month,day_of_week, duration,campaign,pdays, previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed,y)
