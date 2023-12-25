4911cea2ab4b0944c12849c2520e2d0e55591f50
Elif Arslan <95655388+cmpe-elifarslan@users.noreply.github.com>
2023-12-25 20:44:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -220,16 +220,17 @@ if authentication_status == True:
             st.write("calculating...")
             st.write("calculated:")
             
-            if age==31:
+            if result[0]==1:
                 st.subheader("yes")
                 y="yes"
             elif result[0] == 0:
                 st.subheader("no")
                 y="no"
             time.sleep(1)  
-            db.change_data(age,job,marital,education, default,housing,loan,contact,month,day_of_week, duration,campaign,pdays, previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed,y)
+            db.insert_data(age,job,marital,education, default,housing,loan,contact,month,day_of_week, duration,campaign,pdays, previous,poutcome,emp_var_rate,cons_price_idx,cons_conf_idx,euribor3m,nr_employed,y)
             show=db.fetch_all_data() 
             df_print = pd.DataFrame(show)
+            df_print= df_print[df_print['y'] != '-']
             latest_row = df_print[df_print['time'] == df_print['time'].max()]
             st.dataframe(latest_row)
 
@@ -244,5 +245,6 @@ if authentication_status == True:
     if selected=="data view" :
         show=db.fetch_all_data() 
         df = pd.DataFrame(show)
+        df = df[df['y'] != '-']
         df = df.sort_values(by='time')
         st.dataframe(df)
